2ee4195efcc5390ac5c6218688382bd7db1d491e
yxz <3069946891@qq.com>
2026-03-18 16:18:20 +0800

    reset rsm.go

--- src/kvraft1/rsm/rsm.go
+++ src/kvraft1/rsm/rsm.go
@@ -1,46 +1,36 @@
 package rsm
 
 import (
-	"math/rand"
 	"sync"
-	"time"
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labrpc"
-	raft "6.5840/raft1"
+	"6.5840/raft1"
 	"6.5840/raftapi"
-	tester "6.5840/tester1"
+	"6.5840/tester1"
+
 )
 
-const opTimeout = 100 * time.Millisecond
+var useRaftStateMachine bool // to plug in another raft besided raft1
 
-var useRaftStateMachine bool
-var Unregister = struct{}{}
 
 type Op struct {
-	Me  int
-	Id  int64
-	Req any
+	// Your definitions here.
+	// Field names must start with capital letters,
+	// otherwise RPC will break.
 }
 
-// StateMachine is implemented by the replicated server (see server.go).
+
+// A server (i.e., ../server.go) that wants to replicate itself calls
+// MakeRSM and must implement the StateMachine interface.  This
+// interface allows the rsm package to interact with the server for
+// server-specific operations: the server must implement DoOp to
+// execute an operation (e.g., a Get or Put request), and
+// Snapshot/Restore to snapshot and restore the server's state.
 type StateMachine interface {
 	DoOp(any) any
 	Snapshot() []byte
 	Restore([]byte)
-}
-
-type Waiter struct {
-	id   int64
-	term int
-	ch   chan any
-}
-
-func (w *Waiter) notify(res any) {
-	select {
-	case w.ch <- res:
-	default:
-	}
 }
 
 type RSM struct {
@@ -48,28 +38,36 @@ type RSM struct {
 	me           int
 	rf           raftapi.Raft
 	applyCh      chan raftapi.ApplyMsg
-	maxraftstate int
+	maxraftstate int // snapshot if log grows this big
 	sm           StateMachine
-
-	pendings map[int]*Waiter
-
-	shutdownCh   chan struct{}
-	shutdownOnce sync.Once
+	// Your definitions here.
 }
 
+// servers[] contains the ports of the set of
+// servers that will cooperate via Raft to
+// form the fault-tolerant key/value service.
+//
+// me is the index of the current server in servers[].
+//
+// the k/v server should store snapshots through the underlying Raft
+// implementation, which should call persister.SaveStateAndSnapshot() to
+// atomically save the Raft state along with the snapshot.
+// The RSM should snapshot when Raft's saved state exceeds maxraftstate bytes,
+// in order to allow Raft to garbage-collect its log. if maxraftstate is -1,
+// you don't need to snapshot.
+//
+// MakeRSM() must return quickly, so it should start goroutines for
+// any long-running work.
 func MakeRSM(servers []*labrpc.ClientEnd, me int, persister *tester.Persister, maxraftstate int, sm StateMachine) *RSM {
 	rsm := &RSM{
 		me:           me,
 		maxraftstate: maxraftstate,
 		applyCh:      make(chan raftapi.ApplyMsg),
 		sm:           sm,
-		pendings:     make(map[int]*Waiter),
-		shutdownCh:   make(chan struct{}),
 	}
 	if !useRaftStateMachine {
 		rsm.rf = raft.Make(servers, me, persister, rsm.applyCh)
 	}
-	go rsm.reader()
 	return rsm
 }
 
@@ -77,103 +75,16 @@ func (rsm *RSM) Raft() raftapi.Raft {
 	return rsm.rf
 }
 
+
+// Submit a command to Raft, and wait for it to be committed.  It
+// should return ErrWrongLeader if client should find new leader and
+// try again.
 func (rsm *RSM) Submit(req any) (rpc.Err, any) {
-	op := Op{Me: rsm.me, Id: rand.Int63(), Req: req}
-	index, term, isLeader := rsm.rf.Start(op)
-	if !isLeader {
-		return rpc.ErrWrongLeader, nil
-	}
 
-	waiter := &Waiter{id: op.Id, term: term, ch: make(chan any)}
-	rsm.registerWaiter(index, waiter)
-	return rsm.waitForOp(index, waiter)
-}
+	// Submit creates an Op structure to run a command through Raft;
+	// for example: op := Op{Me: rsm.me, Id: id, Req: req}, where req
+	// is the argument to Submit and id is a unique id for the op.
 
-func (rsm *RSM) waitForOp(index int, waiter *Waiter) (rpc.Err, any) {
-	ticker := time.NewTicker(opTimeout)
-	defer ticker.Stop()
-	defer rsm.unregisterWaiter(index, waiter)
-
-	for {
-		select {
-		case res := <-waiter.ch:
-			if res == Unregister || rsm.isTermStale(waiter.term) {
-				return rpc.ErrWrongLeader, nil
-			}
-			return rpc.OK, res
-		case <-ticker.C:
-			if rsm.isTermStale(waiter.term) {
-				return rpc.ErrWrongLeader, nil
-			}
-		case <-rsm.shutdownCh:
-			return rpc.ErrWrongLeader, nil
-		}
-	}
-}
-
-func (rsm *RSM) registerWaiter(index int, waiter *Waiter) {
-	rsm.mu.Lock()
-	defer rsm.mu.Unlock()
-	if old := rsm.pendings[index]; old != nil {
-		old.notify(Unregister)
-	}
-	rsm.pendings[index] = waiter
-}
-
-func (rsm *RSM) unregisterWaiter(index int, waiter *Waiter) {
-	rsm.mu.Lock()
-	defer rsm.mu.Unlock()
-	if current, ok := rsm.pendings[index]; ok && current == waiter {
-		delete(rsm.pendings, index)
-	}
-}
-
-func (rsm *RSM) isTermStale(oldTerm int) bool {
-	currentTerm, isLeader := rsm.rf.GetState()
-	return !isLeader || currentTerm != oldTerm
-}
-
-func (rsm *RSM) reader() {
-	for msg := range rsm.applyCh {
-		if !msg.CommandValid {
-			continue
-		}
-		rsm.handleApply(msg)
-	}
-	rsm.failPending()
-}
-
-func (rsm *RSM) handleApply(msg raftapi.ApplyMsg) {
-	op, ok := msg.Command.(Op)
-	if !ok {
-		return
-	}
-
-	result := rsm.sm.DoOp(op.Req)
-
-	rsm.mu.Lock()
-	defer rsm.mu.Unlock()
-	if waiter, exists := rsm.pendings[msg.CommandIndex]; exists {
-		if waiter.id == op.Id {
-			waiter.notify(result)
-		} else {
-			waiter.notify(Unregister)
-		}
-	}
-}
-
-func (rsm *RSM) failPending() {
-	rsm.mu.Lock()
-	defer rsm.mu.Unlock()
-	for _, waiter := range rsm.pendings {
-		waiter.notify(Unregister)
-	}
-	clear(rsm.pendings)
-	rsm.signalShutdown()
-}
-
-func (rsm *RSM) signalShutdown() {
-	rsm.shutdownOnce.Do(func() {
-		close(rsm.shutdownCh)
-	})
+	// your code here
+	return rpc.ErrWrongLeader, nil // i'm dead, try another server.
 }
